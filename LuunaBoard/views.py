0ea5445507e5dbd26d6550a1e5d35571368552a6
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-24 16:38:07 -0500

    Modify views.py

--- LuunaBoard/views.py
+++ LuunaBoard/views.py
@@ -3,16 +3,16 @@ from flask import request, url_for, render_template
 
 @app.route('/')
 def home():
-    return render_template()
+    return render_template('index.html')
 
 @app.route('/categories')
 def categories():
-    return render_template()
+    return render_template('categories.html')
 
 @app.route('/board/<category>')
 def board(category):
-    return render_template()
+    return render_template('index.html')
 
 @app.route('/board/<category>/thread/<int:thread_id>')
 def thread(category, thread_id):
-    return render_template()
+    return render_template('about.html')
