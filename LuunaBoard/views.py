96f68106c435483f207b6c0aab4df63388b689b6
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-23 19:04:06 -0500

    Modify views.py

--- LuunaBoard/views.py
+++ LuunaBoard/views.py
@@ -9,10 +9,10 @@ def home():
 def categories():
     return "You are at the boards url {}".format(url_for('categories'))
 
-@app.route('/<category>')
+@app.route('/board/<category>')
 def board(category):
     return "You are at the board {}, the url is: {}".format(category, url_for('board'))
 
-@app.route('/<category>/<int:thread_id>')
+@app.route('/board/<category>/thread/<int:thread_id>')
 def thread(category, thread_id):
     return "You are at the board {} watching the thread {}, the url is {}".format(category, thread_id, url_for('thread'))
