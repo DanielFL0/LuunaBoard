ceab19547ff506cebe9c101ddec935e7096f8dec
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-24 15:24:15 -0500

    Modify database connection string

--- LuunaBoard/__init__.py
+++ LuunaBoard/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'njlanfdklaklfmdas'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Franxx9208@localhost:3306/LuunaTest'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:Franxx9208@localhost:3306/luunatest'
 db = SQLAlchemy(app)
 
 import LuunaBoard.views
