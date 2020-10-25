bc0e00bca99076a3ef016e5a39393373fd6a67fd
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-25 14:21:04 -0600

    Modify views.py

--- LuunaBoard/views.py
+++ LuunaBoard/views.py
@@ -1,7 +1,10 @@
 from LuunaBoard import app
+import os
 from flask import request, url_for, render_template, redirect
+from werkzeug.utils import secure_filename
 from LuunaBoard import db
 from LuunaBoard.models import Category, Thread, Comment
+from LuunaBoard.config import MEDIA_FOLDER
 
 @app.route('/')
 def home():
@@ -13,6 +16,11 @@ def categories():
     categories = Category.query.all()
     return render_template('categories.html', categories=categories)
 
+def create_thread(thread_title, thread_content, thread_image, thread_category_id):
+    thread = Thread(title=thread_title, content=thread_content, image=thread_image, category_id=thread_category_id)
+    db.session.add(thread)
+    db.session.commit()
+
 @app.route('/board/<int:category>', methods=['POST', 'GET'])
 def board(category):
     category_result = Category.query.filter_by(id=category).first()
@@ -20,9 +28,9 @@ def board(category):
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-        thread = Thread(title=title, content=content, category_id=category_result.id)
-        db.session.add(thread)
-        db.session.commit()
+        image = request.files['image']
+        image.save(os.path.join(MEDIA_FOLDER, secure_filename(image.filename)))
+        create_thread(title, content, image.filename, category_result.id)
         return redirect(url_for('board', category=category))
     return render_template('board.html', category=category_result, threads=threads)
 
