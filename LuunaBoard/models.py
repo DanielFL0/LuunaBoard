e9725834a423d67aa2ada3f665eb3c43467f52f7
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-24 20:43:22 -0500

    Modify models.py

--- LuunaBoard/models.py
+++ LuunaBoard/models.py
@@ -2,6 +2,13 @@ from datetime import datetime
 from LuunaBoard import db
 from flask_sqlalchemy import SQLAlchemy
 
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return "<Category %r>" % self.name
+
 
 class Thread(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,8 +17,12 @@ class Thread(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     replies = db.Column(db.Integer, default=0)
     image = db.Column(db.String(100), nullable=False, default='default.jpg')
-    category = db.Column(db.String(20), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref='thread', lazy=True)
     comments = db.relationship('Comment', backref='thread', lazy=True)
+
+    def __repr__(self):
+        return "<Thread %r>" % self.title
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +30,9 @@ class Comment(db.Model):
     image = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(50), nullable=True)
     thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
+
+    def __repr__(self):
+        return "<Comment %r>" % self.content
 
 db.create_all()
 db.session.commit()
