98a936cac5ca49aa715929192f02dba58ea8b4c5
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-26 07:36:33 -0600

    Add github API

--- LuunaBoard/views.py
+++ LuunaBoard/views.py
@@ -6,12 +6,25 @@ from LuunaBoard import db
 from LuunaBoard.models import Category, Thread, Comment
 from LuunaBoard.config import MEDIA_FOLDER
 from LuunaBoard.config import ALLOWED_EXTENSIONS
+from LuunaBoard.config import GITHUB_ACCESS_TOKEN
+from github import Github
+
+def fetch_repo():
+    session = Github(GITHUB_ACCESS_TOKEN)
+    repo = session.get_repo('DanielFL0/LuunaBoard')
+    branch = repo.get_branch(branch='main')
+    branch_sha = branch.commit.sha
+    commit = repo.get_commit(sha=branch_sha)
+    commit_author = commit.commit.author.name
+    commit_message = commit.commit.message
+    return branch_sha, commit_author, commit_message
 
 @app.route('/')
 def home():
     threads = Thread.query.all()[-30:]
     threads = threads[::-1]
-    return render_template('index.html', threads=threads)
+    branch, author, message = fetch_repo()
+    return render_template('index.html', threads=threads, branch=branch, author=author, message=message)
 
 @app.route('/categories')
 def categories():
