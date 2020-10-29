8c22d339ecbebe2d102d9b1a27d9333722e32132
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-10-29 13:15:02 -0600

    Modify __init__.py

--- LuunaBoard/__init__.py
+++ LuunaBoard/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from LuunaBoard.config import SECRET_KEY
 from LuunaBoard.config import DB_CONNECTION
+from LuunaBoard.config import MEDIA_FOLDER
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION
+app.config['UPLOAD_FOLDER'] = MEDIA_FOLDER
 db = SQLAlchemy(app)
 
 import LuunaBoard.views
