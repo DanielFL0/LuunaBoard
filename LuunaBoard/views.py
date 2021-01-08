0825bb586e1c4e488f50bceeab27d2d4b78d27d4
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-01-08 13:19:47 -0600

    Modify views.py

--- LuunaBoard/views.py
+++ LuunaBoard/views.py
@@ -1,12 +1,11 @@
 from LuunaBoard import app
 import os
-import sys
-import platform
 from flask import request, url_for, render_template, redirect
 from werkzeug.utils import secure_filename
 from LuunaBoard import db
 from LuunaBoard.models import Category, Thread, Comment
-from LuunaBoard.config import ALLOWED_EXTENSIONS
+from LuunaBoard import db_methods
+from LuunaBoard import utils
 
 @app.errorhandler(404)
 def error_404(e):
@@ -18,103 +17,65 @@ def error_500(e):
 
 @app.route('/')
 def home():
-    threads = Thread.query.all()[-10:]
-    threads = threads[::-1]
+    threads = db_methods.fetch_latest_threads(30)
     return render_template('index.html', threads=threads)
 
 @app.route('/categories')
 def categories():
-    categories = Category.query.all()
+    categories = db_methods.fetch_categories()
     return render_template('categories.html', categories=categories)
 
-def allowed_image(filename):
-    extension = filename.split('.')[-1]
-    if extension in ALLOWED_EXTENSIONS:
-        return True
-    return False
-
-def create_thread(thread_title, thread_content, thread_image, thread_category_id):
-    thread = Thread(title=thread_title, content=thread_content, image=thread_image, category_id=thread_category_id)
-    db.session.add(thread)
-    db.session.commit()
-
-@app.route('/board/<int:category>', methods=['POST', 'GET'])
-def board(category):
+@app.route('/board/<int:category_id>', methods=['POST', 'GET'])
+def board(category_id):
     message = None
-    category_result = Category.query.filter_by(id=category).first()
-    threads = Thread.query.filter_by(category_id=category_result.id).all()[-30:]
-    threads = threads[::-1]
+    category_result = db_methods.fetch_category(category_id)
+    threads = db_methods.fetch_threads(category_result.id, 30)
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
         image = request.files['image']
         if len(title) > 50 or len(content) > 200:
             message = "ERROR: Character limit exceeded"
-        if allowed_image(image.filename):
-            create_thread(title, content, image.filename, category_result.id)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(image.filename)))
         else:
-            if allowed_image(image.filename):
+            if utils.allowed_image(image.filename):
                 filename = secure_filename(image.filename)
-                create_thread(title, content, filename, category_result.id)
+                db_methods.create_thread(title, content, filename, category_result.id)
                 image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             else:
-                create_thread(title, content, 'default.jpg', category_result.id)
-            return redirect(url_for('board', category=category))
+                db_methods.create_thread(title, content, 'default.jpg', category_result.id)
+            return redirect(url_for('board', category_id=category_id))
     return render_template('board.html', category=category_result, threads=threads, message=message)
 
-def create_comment(comment_content, comment_image, comment_email, comment_thread_id):
-    comment = Comment(content=comment_content, image=comment_image, email=comment_email, thread_id=comment_thread_id)
-    db.session.add(comment)
-    db.session.commit()
-
-def update_reply_count(thread_id):
-    comments = Comment.query.filter_by(thread_id=thread_id).all()
-    comments_count = Comment.query.filter_by(thread_id=thread_id).count()
-    thread_result = Thread.query.filter_by(id=thread_id).first()
-    thread_result.replies = comments_count
-    db.session.commit()
-
-@app.route('/board/<int:category>/thread/<int:thread_id>', methods=['POST', 'GET'])
-def thread(category, thread_id):
+@app.route('/board/<int:category_id>/thread/<int:thread_id>', methods=['POST', 'GET'])
+def thread(category_id, thread_id):
     message = None
-    category_result = Category.query.filter_by(id=category).first()
-    thread_result = Thread.query.filter_by(id=thread_id).first()
-    comments = Comment.query.filter_by(thread_id=thread_id).all()
+    category_result = db_methods.fetch_category(category_id)
+    thread_result = db_methods.fetch_thread(thread_id)
+    comments = db_methods.fetch_comments(thread_id)
     if request.method == 'POST':
         content = request.form['content']
         image = request.files['image']
         email = request.form['email']
-        if len(content) > 200 or len(content) == 0:
+        if len(content) > 50 or len(content) > 200:
             message = "ERROR: Character limit exceeded"
-        if allowed_image(image.filename):
-            create_comment(content, image.filename, email, thread_result.id)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(image.filename)))  
         else:
-            if allowed_image(image.filename):
+            if utils.allowed_image(image.filename):
                 filename = secure_filename(image.filename)
-                create_comment(content, filename, email, thread_result.id)
+                db_methods.create_comment(content, filename, email, thread_result.id)
                 image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  
             else:
-                create_comment(content, 'default.jpg', email, thread_result.id)
-            update_reply_count(thread_id)               
-            return redirect(url_for('thread', category=category, thread_id=thread_id))
+                db_methods.create_comment(content, 'default.jpg', email, thread_result.id)
+            db_methods.update_reply_count(thread_id)               
+        return redirect(url_for('thread', category_id=category_id, thread_id=thread_id))
     return render_template('thread.html', category=category_result, thread=thread_result, comments=comments, message=message)
 
-@app.route('/board/<int:category>/archive')
-def archive(category):
-    category_result = Category.query.filter_by(id=category).first()
-    threads = Thread.query.filter_by(category_id=category_result.id).all()[-100:]
-    threads = threads[::-1]
+@app.route('/board/<int:category_id>/archive')
+def archive(category_id):
+    category_result = db_methods.fetch_category(category_id)
+    threads = db_methods.fetch_threads(category_result.id, 30)
     return render_template('archive.html', category=category_result, threads=threads)
-
-def fetch_system():
-    python_version = sys.version
-    system = platform.system()
-    operating_system = platform.platform()
-    return python_version, "{}: {}".format(system, operating_system)
 
 @app.route('/about')
 def about():
-    python_version, operating_system = fetch_system()
+    python_version, operating_system = db_methods.fetch_system_info()
     return render_template('about.html', python_version=python_version, operating_system=operating_system)
